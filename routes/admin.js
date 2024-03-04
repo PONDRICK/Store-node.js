d4f8da2075900dda1d5a9a9b2e828fd2252c9d8e
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-03-04 14:53:23 +0700

    /admin + get

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   console.log("Say Hi from store");
   res.send(
-    '<body><form action ="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form></body>'
+    '<body><form action ="/admin/add-product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form></body>'
   );
 });
 
-router.use("/product", (req, res, next) => {
+router.use("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
