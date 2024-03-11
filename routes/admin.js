5eb05521867d95d3604915b4f35c3c6cffc0b2d1
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-03-11 15:03:09 +0700

    ejs

--- routes/admin.js
+++ routes/admin.js
@@ -3,18 +3,28 @@ const path = require("path");
 const express = require("express");
 
 const router = express.Router();
+const adminData = require('./admin')
 
 const rootDir = require("../util/path");
 
+const products = []
+
 router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(__dirname, "../views/add-product.html"));
-  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	const products = adminData.products
+	res.render("add-product", {
+		title: "Add Product (ejs)",
+	});
 });
+// router.get("/add-product", (req, res, next) => {
+// 	// res.sendFile(path.join(__dirname, "../views/add-product.html"));
+// 	// res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+// 	res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// });
 
 router.use("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+	products.push({title: req.body.title});
+	res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,21 @@ const path = require("path");
 const express = require("express");
 
 const router = express.Router();
+const adminData = require('./admin')
 
 const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  // res.sendFile(path.join(__dirname, "../views/shop.html"));
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+	const products = adminData.products
+	res.render("shop", {
+		prods: products,
+		title: "Shop(ejs)",
+	});
 });
+
+// router.get("/", (req, res, next) => {
+// 	// res.sendFile(path.join(__dirname, "../views/shop.html"));
+// 	res.sendFile(path.join(rootDir, "views", "shop.html"));
+// });
 
 module.exports = router;
