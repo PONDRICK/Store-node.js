a91cc6e37fbe3734ad5ec8befe7eed3f7ac6da5a
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-03-11 11:49:58 +0700

    path 2

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,18 @@ const path = require("path");
 const express = require("express");
 
 const router = express.Router();
+
 const rootDir = require("../util/path");
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "../", "views", "add-product.html"));
+  // res.sendFile(path.join(__dirname, "../views/add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
-router.post("/", (req, res, next) => {
-  console.log("req.body");
+
+router.use("/add-product", (req, res, next) => {
+  console.log(req.body);
   res.redirect("/");
 });
+
 module.exports = router;
